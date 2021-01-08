39ea95e918e4937a0468b334fe3b5dfc267252c1
Mechztcc <dojathan@gmail.com>
2021-01-08 18:32:37 -0300

    Search order location

--- src/Orders/index.tsx
+++ src/Orders/index.tsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
 import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
 
 import './styles.css';
-import { Product } from './types';
+import { OrderLocationdata, Product } from './types';
 
 function Orders() {
 
     const [products, setProducts] = useState<Product[]>([]);
     console.log(products);
+
+    const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
     
 
 
@@ -22,6 +25,7 @@ function Orders() {
         <div className="orders-container">
             <StepsHeader />
             <ProductsList products={products}/>
+            <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
         </div>
     )
 }
