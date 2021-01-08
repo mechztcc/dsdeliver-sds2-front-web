a69ef9d4e2d13b96910dbc91219abe665d660926
Mechztcc <dojathan@gmail.com>
2021-01-08 10:23:36 -0300

    Add home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 
 import NavBar from './Navbar/index';
-
+import Home from './Home/index';
 
 function App() {
   return (
-    <div>
+    <>
       <NavBar />
-    </div>
+      <Home />
+    </>
   );
 }
 
