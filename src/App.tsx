4f8e27ae181887f246a228039c7d8bec98b70fdf
Mechztcc <dojathan@gmail.com>
2021-01-08 09:19:32 -0300

    Initial configs

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <p>Lets go Code!</p>
+    </div>
+  );
+}
+
+export default App;
